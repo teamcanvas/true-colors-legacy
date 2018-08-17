9a5105adec144c372637db3cf2c0c166eb110509
JuunKi Jin <me@atko.space>
2018-08-17 20:15:59 +0900

    Add cm1106 test code

--- main/true_colors_main.c
+++ main/true_colors_main.c
@@ -7,7 +7,24 @@
 
 #include <stdio.h>
 
+#include "esp_err.h"
+#include "esp_log.h"
+
+#include "i2c_api.h"
+#include "cm1106.h"
+
 void app_main()
 {
-    printf("hello world!");
+	i2c_api_master_init();
+	esp_err_t err;
+	while(1) {
+		uint16_t value = 0;
+		err = cm1106_measure_result(&value);
+		if (err == ESP_OK) {
+			printf("CM1106 Value :%d\n", value);
+		} else {
+			printf("CM1106 measurement failed");
+		}
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
 }
