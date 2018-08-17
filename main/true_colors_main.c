bfa179f2b9b6680ce992cbc51c720c9ba7d4f2f6
JuunKi Jin <me@atko.space>
2018-08-17 23:17:45 +0900

    Modify test print function

--- main/true_colors_main.c
+++ main/true_colors_main.c
@@ -21,9 +21,9 @@ void app_main()
 		uint16_t value = 0;
 		err = cm1106_measure_result(&value);
 		if (err == ESP_OK) {
-			printf("CM1106 Value :%d\n", value);
+			ESP_LOGI("CM1106 Value :%d\n", value);
 		} else {
-			printf("CM1106 measurement failed");
+			ESP_LOGI("CM1106 measurement failed");
 		}
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
