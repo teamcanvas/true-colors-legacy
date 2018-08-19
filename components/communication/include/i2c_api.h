cf6507c63601825bf17f8ab9fb4b0599d347b39d
JuunKi Jin <me@atko.space>
2018-08-20 07:30:26 +0900

    Modify define more pretty

--- components/communication/include/i2c_api.h
+++ components/communication/include/i2c_api.h
@@ -11,20 +11,20 @@
 #include "esp_err.h"
 #include "driver/i2c.h"
 
-#define I2C_MASTER_USER_NUM I2C_NUM_1
-#define I2C_MASTER_DATA_BUF_LEN 512
+#define I2C_MASTER_USER_NUM 		I2C_NUM_1
+#define I2C_MASTER_DATA_BUF_LEN 	512
 
-#define I2C_MASTER_SDA_PIN 33
-#define I2C_MASTER_SCL_PIN 32
+#define I2C_MASTER_SDA_PIN 			GPIO_NUM_33
+#define I2C_MASTER_SCL_PIN 			GPIO_NUM_32
 
-#define I2C_MASTER_TX_BUF_DISABLE 0
-#define I2C_MASTER_RX_BUF_DISABLE 0
-#define I2C_MASTER_FREQ_HZ 400000
+#define I2C_MASTER_TX_BUF_DISABLE 	0
+#define I2C_MASTER_RX_BUF_DISABLE 	0
+#define I2C_MASTER_FREQ_HZ 			400000
 
-#define I2C_ACK_CHECK_ENABLE 0x1
-#define I2C_ACK_CHECK_DISABLE 0x0
-#define I2C_ACK_VAL 0x0
-#define I2C_NACK_VAL 0x1
+#define I2C_ACK_CHECK_ENABLE 		0x1
+#define I2C_ACK_CHECK_DISABLE 		0x0
+#define I2C_ACK_VAL 				0x0
+#define I2C_NACK_VAL 				0x1
 
 esp_err_t i2c_api_master_init();
 esp_err_t i2c_api_master_write_bytes(uint8_t addr, uint8_t *data, size_t length);
