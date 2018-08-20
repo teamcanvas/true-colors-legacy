e0424ff6260fe97173d3b555914be3d8baba2225
JuunKi Jin <me@atko.space>
2018-08-20 09:09:35 +0900

    Add pm2006 debug print

--- main/true_colors_main.c
+++ main/true_colors_main.c
@@ -10,17 +10,22 @@
 #include "esp_err.h"
 #include "esp_log.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "i2c_api.h"
+#include "uart_zero_api.h"
+
 #include "cm1106.h"
+#include "pm2006.h"
 
 #define TRUE_COLORS_TAG "TRUE COLORS"
 
-void app_main()
-{
-	i2c_api_master_init();
+void cm1106_task(void *parameter) {
+	uint16_t value = 0;
 	esp_err_t err;
+
 	while(1) {
-		uint16_t value = 0;
 		err = cm1106_measure_result(&value);
 		if (err == ESP_OK) {
 			ESP_LOGI(TRUE_COLORS_TAG, "CM1106 Value :%d\n", value);
@@ -29,4 +34,30 @@ void app_main()
 		}
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
+}
+
+void pm2006_task(void *parameter) {
+	uint16_t value = 0;
+	esp_err_t err;
+
+	pm2006_open_measurement();
+
+	while(1) {
+		err = pm2006_read_particle_measurement(&value);
+		if (err == ESP_OK) {
+			ESP_LOGI(TRUE_COLORS_TAG, "PM2006 Value :%d\n", value);
+		} else {
+			ESP_LOGI(TRUE_COLORS_TAG, "PM2006 measurement failed");
+		}
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main()
+{
+	i2c_api_master_init();
+	uart_zero_api_init();
+
+    xTaskCreate(&cm1106_task, "cm1106_task", 2048, NULL, 1, NULL);
+    xTaskCreate(&pm2006_task, "pm2006_task", 2048, NULL, 1, NULL);
 }
